e7d02ed4b0d05a098a2e3f00da2b6ebfba9e6730
shuhratormonaliyev <shuratormonaliyev7@gmail.com>
2024-06-03 13:58:08 +0500

    lucky

---
+++ js/index.js
@@ -0,0 +1,239 @@
+//==================  1 - MISOL ======================
+
+// function sumArr(arg) {
+//     let sum = 0;
+//     for(let i = 0; i < arg.length; i++) {
+//         sum = sum + arg[i]
+//     }
+//     return sum;
+// }
+// console.log(sumArr([1, 2, 3]));
+
+
+// ======================  2 - MISOL ======================
+
+
+
+// function num(num1 , num2) {
+//     let = max;
+//     if(num1 > num2) {
+//         max = num1;
+//     } else {
+//         max = num2;
+//     }
+//     return max;
+// }
+// let result = num(9 , 5);
+// console.log(result);
+
+
+
+// ========================= 3 - MISOL =====================
+
+// function num(num1 , num2) {
+//         let = max;
+//         if(num1 < num2) {
+//             max = num2;
+//         } else {
+//             max = num1;
+//         }
+//         return max;
+//     }
+//     let result = num(9 , 5);
+//     console.log(result);
+
+
+
+// ========================= 4 - MISOL =====================
+
+// function positivizeArray(arr) {
+//     return arr.map(function(item) {
+//       return Math.abs(item);
+//     });
+//   }
+//   let numbers = [-1, -5, 6, -3, 2, -8, 10];
+//   numbers = positivizeArray(numbers);
+//   console.log(numbers);
+
+
+
+//  ===================== 5 - MISOL =======================
+
+
+// function doubleArrayElements(arr) {
+//     return arr.map(function(item) {
+//       return item * 2;
+//     });
+//   }
+//   let numbers = [1, 2, 3, 4, 5];
+//   numbers = doubleArrayElements(numbers);
+  
+//   console.log(numbers);
+
+
+
+//  ==================== 6 - MISOL =========================
+// function juftyoqqil(arg) {
+//     let b = [] ; 
+//     for(let i = 0 ; i < arg.length ; i++){
+//     if(arg[i] % 2 == 1){
+//     b.push(arg[i])
+//     }
+//     }
+//     console.log(b);
+// }
+// console.log(juftyoqqil([1,2,3,4,5,6,7,8,9]));
+
+// ================= 7- MISOL  ================
+// function toqyoqqil(arg) {
+//     let b = [] ; 
+//     for(let i = 0 ; i < arg.length ; i++){
+//     if(arg[i] % 2 == 0){
+//     b.push(arg[i])
+//     }
+//     }
+//     console.log(b);
+// }
+// console.log(toqyoqqil([1,2,3,4,5,6,7,8,9]));
+
+// ================== 8-MISOL  ==============
+// function borligit(arg) {
+//     let a = 7;
+//     for(let i = 0 ; i < arg.length ; i++ ){
+//     if(a == arg[i]){
+//     console.log('bunday qiymat mavjud');
+//     }
+//     }
+//     return borligit ;
+// }
+// console.log(borligit([1,2,3,4,5,6,7,8,9,10]));
+
+// // ================ 9-MISOL  ====================
+// function teskaritartib(arg) {
+//     for(let i = arg.length ; i >= 0 ; i--){
+//     console.log(arg[i]);
+//     }
+// }
+// console.log(teskaritartib([1,2,3,4,5,6,7,8,9,10]));
+
+
+
+// =============== 10 - MISOL =========================
+
+
+
+// ================ 11- MISOL ========================
+// function eplus1(arg) {
+//     let a = [] ;
+//     for(let i = 0 ; i < arg.length ; i++){
+//     a.push(arg[i]+1);
+//     }
+//     console.log(a);
+// }
+// console.log(eplus1([1,2,3,4,5,6,7,8,9]));
+
+// ==================== 12- MISOL ==============
+// function berElmInd(arg) {
+//     let a = 5;
+//     for(let i = 0 ; i < arg.length ; i++){
+//     if(a === arg[i]){
+//     console.log(i);
+//     }
+//     }
+// }
+// console.log(berElmInd([1,2,3,4,5,6,7,8,9,10]));
+
+//================ 13 - MISOL ====================
+// function berElmInd(arg) {
+//     let a = 5;
+//     let counter = 0 ;
+//     for(let i = 0 ; i < arg.length ; i++){
+//     if(a === arg[i]){
+//     counter += 1 ;
+//     }
+//     }
+// console.log(counter);
+// }
+// console.log(berElmInd([1,5,3,4,5,6,5,8,9,5]));
+
+//================ 14 - MISOL ====================
+// function musbatsonlarmassivi(arg) {
+//     let newmassiv = [ ] ;
+//     for(let i = 0 ; i < arg.length ; i++){
+//     if(arg[i] > 0 ){
+//     newmassiv.push(arg[i]);
+//     }
+//     }
+//     console.log(newmassiv);
+// }
+// console.log(musbatsonlarmassivi([1,-2,3,-4,5,-6,7,-8,9,-10]));
+
+//================ 15 - MISOL ====================
+// function manfiySonlarMassivi(arg) {
+//     let newmassiv = [ ] ;
+//     for(let i = 0 ; i < arg.length ; i++){
+//     if(arg[i] < 0 ){
+//     newmassiv.push(arg[i]);
+//     }
+//     }
+//     console.log(newmassiv);
+// }
+// console.log(manfiySonlarMassivi([1,-2,3,-4,5,-6,7,-8,9,-10]));
+
+//================ 16 - MISOL ====================
+// function satr(arg) {
+//     for(let i = 0 ; i < arg.length ; i++ ){
+//     console.log(arg[i]);
+//     }
+// }
+// console.log(satr([1,2,3,4,5,6,7,8,9]));
+
+//================ 17 - MISOL ====================
+// function kvadrat(arg) {
+//     let a = [] ;
+//     for( let i = 0 ; i < arg.length ; i++ ){
+//     a.push(arg[i]*arg[i]);
+//     }
+//     console.log(a);
+// }
+// console.log(kvadrat([1,2,3,4,5,6,7,8,9,11,22,33,44,55,66,77,88,99]));
+//================ 18 - MISOL ====================
+// function a(arg) {
+//     let b = ['massiv'] ;
+//     for(let i = 0 ; i < arg.length ; i++){
+//     b.push(arg[i]);
+//     }
+//     console.log(b);
+// }
+// console.log(a([1,2,3,4,5,6,7,8,9]));
+
+
+//   ===================== 19 - MISOL =================
+
+
+// function calculate(arr) {
+//     let sum = arr.one(function(max, min) {
+//       return max + min;
+//     }, 0);
+//     let average = sum / arr.length;
+//     return { sum: sum, average: average };
+//   }
+//   let numbers = [10, 20, 30, 40, 50];
+//   let result = calculate(numbers);
+  
+//   console.log(result);
+// 
+
+
+
+//   ====================== 20 - MISOL ===================
+// let = prompt('Massivdagi barcha takroriy elementlarni olib tashlaydigan funksiya yozing');
+
+
+// let arr = [1, 2, 3, 4, 5];
+// function name(arr) {
+//     for(let i = 0; i = arr.length; i++)
+//     console.log(arr(arr.length - 1));
+// // }
+
+
